39b81925c9faa6d2c5e0a31ee41d72202bfc41f7
DJ <dhrubajyotipaul3@gmail.com>
2022-11-12 15:37:52 -0600

    Done with Home page and css stuff

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -4,9 +4,8 @@ export default function Home() {
     return (
         <div class="center">
             <h1>Welcome to Face Attendance!</h1>
-            {/* TODO: FIX URL!!! */}
             <Link to="/AdminHome" className="button button1">Teacher Login</Link>
-            <Link to="/StudentUpload" className="button button2">Student Login</Link>
+            <Link to="/StudentHome" className="button button2">Student Login</Link>
         </div>
     )
 }
