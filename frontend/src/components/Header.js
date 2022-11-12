b41faefff5ca2ca22509c331a44fbe2d2ba54a42
sameer-n012 <sameer.narendran@gmail.com>
2022-11-12 14:23:27 -0600

    added bootstrap and created header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,27 @@
+import { Container, Button } from 'react-bootstrap'
+
 export default function Header({page}) {
 
     // TODO insert application name
 	return (
-		<div className='header' backgroundColor='#990000'>
-			<h2 className='header-title' color='#ffffff'>Face Attendance</h2>
+		<Container fluid className='header w-100 p-3 d-flex  align-items-center' style={{
+            backgroundColor: '#990000',
+        }}>
+			<h2 className='header-title text-white w-50'>Face Attendance</h2>
             {page == 'student_home' ? (
-                <div>
-                    <button className='header-button'>Join Class</button>
-                    <button className='header-button'>Upload Image</button>
-                </div>
+                <Container className='w-50 d-flex align-items-center justify-content-end'>
+                    <Button className='header-button m-2'>Join Class</Button>
+                    <Button className='header-button m-2'>Upload Image</Button>
+                </Container>
             ) : page == 'teacher_home' ? (
-                <div>
-                    <button className='header-button'>Create Class</button>
-                </div>
+                <Container className='w-50 d-flex align-items-center justify-content-end'>
+                    <Button className='header-button m-2'>Create Class</Button>
+                </Container>
             ) : (
-                <div>
+                <Container>
                     
-                </div>
+                </Container>
             )}
-		</div>
+		</Container>
 	);
 }
