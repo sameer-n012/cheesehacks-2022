723fc45f38d1f5d77303f2d7949e738fd35a185c
Yogesh Prabhu <yogeshpra318@gmail.com>
2022-11-12 14:21:14 -0600

    background color

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,8 +1,9 @@
 export default function Home() {
 
     return (
-        <div>
+        <div className="homepage">
             <h1>This is the home page</h1>
+            
         </div>
     )
 
