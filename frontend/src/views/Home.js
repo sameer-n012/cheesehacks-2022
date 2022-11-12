35f202cd164d62a64f4c934ee1e50a64f91c1b1f
DJ <dhrubajyotipaul3@gmail.com>
2022-11-12 15:16:59 -0600

    Developed the Home Page (with css code)

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,8 +1,11 @@
 export default function Home() {
 
     return (
-        <div>
-            <h1>This is the home page</h1>
+        <div class="center">
+            <h1>Welcome to Face Attendance!</h1>
+            {/* TODO: FIX URL!!! */}
+            <a href="https://google.com" class="button button1">Teacher Login</a>
+            <a href="https://google.com" class="button button2">Student Login</a>
         </div>
     )
 
