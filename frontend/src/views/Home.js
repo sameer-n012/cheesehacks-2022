4d5ef3d605a6e43d10abf3e6f07218a2d1876f9e
Yogesh Prabhu <yogeshpra318@gmail.com>
2022-11-12 20:18:02 -0600

    -

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,9 +1,13 @@
 import {Link} from 'react-router-dom';
+import { Container, Accordion, Form} from 'react-bootstrap'
+
 export default function Home() {
 
     return (
         <div class="center">
             <h1>Welcome to Face Attendance!</h1>
+
+           
             <Link to="/AdminHome" className="button button1">Teacher Login</Link>
             <Link to="/Student" className="button button2">Student Login</Link>
         </div>
