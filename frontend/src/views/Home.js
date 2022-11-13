14788f08b282e8dd2d5ca2aac6e31f60def8ccca
DJ <dhrubajyotipaul3@gmail.com>
2022-11-13 03:54:16 -0600

    .

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,6 +1,11 @@
 import {Link} from 'react-router-dom';
 
 export default function Home() {
+
+    const handleEmailChange = (e) => {
+        localStorage.setItem('current_userid', e.target.value);
+    }
+
     return (
         <div class="center">
             <h1>Welcome to Face Attendance!</h1>
@@ -9,7 +14,7 @@ export default function Home() {
             {/* Form Padding  */}
             <form className='form' action="/api/email_check" method="POST">
                 <label for="email">Email:</label>
-                <input type="text" id="email" name="email"></input>
+                <input type="text" id="email" name="email" onChange={(e) => {handleEmailChange(e)}}></input>
                 <input type="submit" value="Login" className='button submit m-3'></input>
                 <br></br>
                 <button className='button button1 m-2' formAction='/api/teacher_sign_up'> Teacher Sign Up </button>
