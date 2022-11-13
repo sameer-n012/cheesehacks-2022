5a49b9d8159131046b8dcc9fcd713fdad964321a
CannonWilson <CannonGWilson@gmail.com>
2022-11-13 09:27:03 -0600

    Styling for upload page

--- frontend/src/views/UploadForm.js
+++ frontend/src/views/UploadForm.js
@@ -1,11 +1,19 @@
+import '../App.css'
+import Form from 'react-bootstrap/Form';
+
 export default function UploadForm() {
 
     return (
         <div>
-            <h2>This is the upload page!</h2>
             <form method="POST" action="/api/upload" enctype="multipart/form-data">
-                <input type="file" name="file" />
-                <input type="submit" value="Upload" />
+                <div className="d-flex flex-column align-items-center">
+                    
+                    <Form.Group controlId="formFileLg" className="mb-3">
+                        <Form.Control type="file" name="file" size="lg" />
+                    </Form.Group>
+                    
+                    <input type="submit" value="Upload" className="btn btn-outline-dark w-25 m-3"/>
+                </div>
             </form>
         </div>
     )
