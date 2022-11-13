f0ec5f12cccc826dd43930af09124b511e416739
DJ <dhrubajyotipaul3@gmail.com>
2022-11-13 00:38:28 -0600

    Fixed Padding

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -20,19 +20,16 @@ export default function Home() {
             <h1>Welcome to Face Attendance!</h1>
             <br></br>
             
-            {/* Form Padding 
+            {/* Form Padding  */}
             <form action="/api/email_check" method="POST">
                 <label for="email">Email:</label>
                 <input type="text" id="email" name="email"></input>
-                <br></br>
-                <input type="submit" value="Login" className='button submit' onClick={sendMessage}></input>
+                <input type="submit" value="Login" className='button submit m-3'></input>
             </form>
-            */}
-
 
             {/* Sign Up Buttons */}
-            <Link to="/admin" className="button button1">Teacher Sign Up</Link>
-            <Link to="/student" className="button button2">Student Sign Up</Link>
+            <Link to="/admin" className="button button1 m-2">Teacher Sign Up</Link>
+            <Link to="/student" className="button button2 m-2">Student Sign Up</Link>
         </div>
     )
 }
