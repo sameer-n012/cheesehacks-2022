20d3e53ade789a1caf4a019fb3f28c9133fc508e
sameer-n012 <sameer.narendran@gmail.com>
2022-11-13 03:18:10 -0600

    small header changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -58,7 +58,7 @@ export default function Header({page}) {
             body: className
         };
 
-        fetch('/api/join-class', requestOptions).then(
+        fetch('/api/join-class?userid=' + currentUser, requestOptions).then(
             response => response.status 
         ).then(
             status => { console.log(status) }
@@ -93,12 +93,12 @@ export default function Header({page}) {
 		<Container fluid className='header w-100 p-3 d-flex  align-items-center uw-red-bg'>
 			<Link to='/' style={{width:'inherit'}}><h2 className='header-title text-white text-decoration-none'>Face Attendance</h2></Link>
             <Container className='d-flex align-items-center justify-content-end'>
-                {page == 'student_home' ? (
+                {page == 'student_home' && currentUser ? (
                     <div>
                         <Button variant='outline-light' className='header-button m-2' onClick={() => {handleAddShow()}}>Join Class</Button>
                         <Button variant='outline-light' className='header-button m-2' onClick={() => {uploadImage()}}>Upload Image</Button>
                     </div>
-                ) : page == 'teacher_home' ? (
+                ) : page == 'teacher_home' && currentUser ? (
                     <div>
                         <Button variant='outline-light' className='header-button m-2' onClick={() => {handleCreateShow()}}>Create Class</Button>
                     </div>
