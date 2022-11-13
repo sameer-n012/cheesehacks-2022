e6308edf995875e3e8508970e2ac4a4fc8889ec8
CannonWilson <CannonGWilson@gmail.com>
2022-11-13 04:45:57 -0600

    Image upload now converts to embeddings. Error handling in image detection

--- backend/server.py
+++ backend/server.py
@@ -5,6 +5,7 @@ from markupsafe import escape
 import os
 import numpy as np
 import cv2
+import torch
 from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
 import cv2
@@ -25,6 +26,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 class_codes = []
 known_face_encodings = []
 known_face_names = [] # For now, just append file names as names
+image_list = []
 
 # If required, create a face detection pipeline using MTCNN:
 mtcnn = MTCNN(image_size=160, margin=0)
@@ -32,24 +34,38 @@ mtcnn = MTCNN(image_size=160, margin=0)
 # Create an inception resnet (in eval mode):
 resnet = InceptionResnetV1(pretrained='vggface2').eval()
 
-def make_images_and_encodings():
 
-    for file in os.listdir(UPLOAD_FOLDER):
+def user_imgurls():
+    user_url_list = []
+    with open('users.csv') as f:
+        reader = csv.reader(f)
+        users_csv_data = list(reader)
+        header = users_csv_data[0]
+        rows = users_csv_data[1:]
 
-        img = Image.open(os.path.join(UPLOAD_FOLDER, file))
-        # Get cropped and prewhitened image tensor
-        img_cropped = mtcnn(img)
+        for i in range(len(rows)):
+            # Add urls from user
+            user_urls = rows[i][header.index('imgUrls')].split(",")
+            user_url_list.extend(user_urls)
+    return user_url_list
 
-        if img_cropped is not None:
 
-            # Calculate embedding (unsqueeze to add batch dimension)
-            img_embedding = resnet(img_cropped.unsqueeze(0))
-            known_face_encodings.append(img_embedding)
-            known_face_names.append(file)
+def get_images_embeddings():
+
+    #url_list = user_imgurls()
+    url_list = os.listdir(UPLOAD_FOLDER)
+
+    for url in url_list:
+        print("URL " + url)
+        suffix = url.split('.')[-1]
+        prefix = url.split('.')[0]
+        if suffix == "npy":
+            print('added')
+            known_face_encodings.append(np.load(os.path.join(UPLOAD_FOLDER, url)))
         else:
-            print('got NONE embedding')
-
-make_images_and_encodings()
+            image_list.append(os.path.join(UPLOAD_FOLDER, url))
+        if prefix not in known_face_names:
+                known_face_names.append(prefix)
 
 
 app = Flask(__name__, static_folder='../frontend/build')
@@ -106,35 +122,71 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def user_emails_for_class_code(class_code):
+    user_email_list = []
+    with open('users.csv') as f:
+        reader = csv.reader(f)
+        users_csv_data = list(reader)
+        header = users_csv_data[0]
+        rows = users_csv_data[1:]
+
+        for i in range(len(rows)):
+            class_list = rows[i][header.index('classes')].split(",")
+            if (class_code in class_list):
+                user_email = rows[i][header.index('email')]
+                user_email_list.append(user_email)
+    return user_email_list
+
+
+
 @app.route('/api/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
 
-        # Check if the requst has the class code
-        if 'class_code' not in request.form:
-            flash('No class code')
-            return redirect(request.url)
-        class_code = request.form['class_code']
-
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
+            print('No file part')
             return redirect(request.url)
         file = request.files['file']
+
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
-            flash('No selected file')
+            print('No selected file')
             return redirect(request.url)
+
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            desired_path = os.path.join(UPLOAD_FOLDER, class_code)
-            if not os.path.exists(desired_path):
-                flash('Invalid class code')
-                return redirect(request.url)
-            file.save(os.path.join(desired_path, filename))
-            return redirect('/upload')
+            prefix = filename.split(".")[0]
+            desired_path_img = os.path.join(UPLOAD_FOLDER, filename)
+            desired_path_embed = os.path.join(UPLOAD_FOLDER, f'{prefix}.npy')
+            
+            # Generate and save the embedding
+            img = Image.open(file).convert('RGB')
+            # Get cropped and prewhitened image tensor
+            img_cropped = mtcnn(img)
+
+            if img_cropped is not None:
+                # Remove existing files if name conflicts
+                if os.path.exists(desired_path_img):
+                    os.remove(desired_path_img)
+                    if os.path.exists(desired_path_embed):
+                        os.remove(desired_path_embed)
+                file.save(desired_path_img)
+
+                # Calculate embedding (unsqueeze to add batch dimension)
+                img_embedding = resnet(img_cropped.unsqueeze(0))
+                np.save(desired_path_embed, img_embedding.detach().numpy())
+            
+            else:
+                print('Embedding failed')
+                flash('Embedding failed')
+
     return redirect('/upload')
+
+### TODO: implement
+def mark_as_present(name):
+    pass
 
 
 
@@ -144,53 +196,42 @@ def upload_file():
 @app.route('/api/detect/<class_code>', methods=['POST', 'GET'])
 @cross_origin()
 def detect_face_from_img(class_code):
-    if request.method == 'POST':
-        # print(request.form['image'])
-        # img = Image.open(BytesIO(base64.b64decode(request.form['image'])))
-        file = request.form['image']
-        starter = file.find(',')
-        image_data = file[starter+1:]
-        image_data = bytes(image_data, encoding="ascii")
-        img = Image.open(BytesIO(base64.b64decode(image_data))).convert('RGB')
+    try:
+        if request.method == 'POST':
+            # print(request.form['image'])
+            # img = Image.open(BytesIO(base64.b64decode(request.form['image'])))
+            file = request.form['image']
+            starter = file.find(',')
+            image_data = file[starter+1:]
+            image_data = bytes(image_data, encoding="ascii")
+            img = Image.open(BytesIO(base64.b64decode(image_data))).convert('RGB')
 
-        """
-        nparr = np.fromstring(request.data, np.uint8) # convert string of image data to uint8
-        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # decode image
-        #face_encoding = face_recognition.face_encodings(img)[0] # Get first match only
-        """
-        img_cropped = mtcnn(img)
-        # Calculate embedding (unsqueeze to add batch dimension)
-        img_embedding = resnet(img_cropped.unsqueeze(0))
+            img_cropped = mtcnn(img)
+            # Calculate embedding (unsqueeze to add batch dimension)
+            img_embedding = resnet(img_cropped.unsqueeze(0))
 
-        # matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-        distance = None
-        cos = CosineSimilarity()
-        min_idx = None
-        for idx in range(len(known_face_encodings)):
-            cur_embedding = known_face_encodings[idx]
-            output = cos(cur_embedding, img_embedding)[0].item()
-            if distance is None or output < distance:
-                distance = output
-                min_idx = idx
-        if min_idx is not None:
-            name = known_face_names[min_idx]
-            return name
-        else:
-            return 'FAILED'
-
-        
-        """
-        name = ""
-        face_distance = face_recognition.face_distance(known_face_encodings, face_encoding)
-        best_match_idx = np.argmin(face_distance)
-        if matches[best_match_idx]:
-            name = known_face_names[best_match_idx]
-        if name in known_face_names:
-            print(name +" is here")
-        
-            return Response("{'foundUser':" + name + "}", status=200, mimetype='application/json')
-        """
-    # return Response("{'foundUser':'None'}", status=404, mimetype='application/json')
+            # matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            distance = None
+            cos = CosineSimilarity()
+            min_idx = None
+            get_images_embeddings() # don't mess with class code
+            print(f"LEN:  {len(known_face_encodings)}")
+            for idx in range(len(known_face_encodings)):
+                cur_embedding = torch.from_numpy(known_face_encodings[idx])
+                output = cos(cur_embedding, img_embedding)[0].item()
+                if distance is None or output < distance:
+                    distance = output
+                    min_idx = idx
+            if min_idx is not None:
+                name = known_face_names[min_idx]
+                mark_as_present(name)
+                return name
+            else:
+                return 'FAILED'
+    except Exception as e:
+        print('error occurred during detection')
+        print(e)
+        return 'FAILED'
 
 
 ### Email Check route
--- frontend/src/views/UploadForm.js
+++ frontend/src/views/UploadForm.js
@@ -4,7 +4,6 @@ export default function UploadForm() {
         <div>
             <h2>This is the upload page!</h2>
             <form method="POST" action="/api/upload" enctype="multipart/form-data">
-                <input name="class_code" placeholder="Class code: "/>
                 <input type="file" name="file" />
                 <input type="submit" value="Upload" />
             </form>
