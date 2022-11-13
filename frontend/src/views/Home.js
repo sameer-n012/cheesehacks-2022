ad9c1e1111b6c080e3f2774deab942f2e47fcf29
DJ <dhrubajyotipaul3@gmail.com>
2022-11-12 20:14:55 -0600

    checking email still...

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -4,8 +4,18 @@ export default function Home() {
     return (
         <div class="center">
             <h1>Welcome to Face Attendance!</h1>
-            <Link to="/AdminHome" className="button button1">Teacher Login</Link>
-            <Link to="/Student" className="button button2">Student Login</Link>
+            
+            {/* Form Padding */}
+            <form action="/api/email_check" method="POST">
+                <label for="email">Email:</label>
+                <input type="text" id="email" name="email"></input>
+                <br></br>
+                <input type="submit" value="Login" className='button submit'></input>
+            </form>
+
+            {/* Sign Up Buttons */}
+            <Link to="/AdminHome" className="button button1">Teacher Sign Up</Link>
+            <Link to="/Student" className="button button2">Student Sign Up</Link>
         </div>
     )
 }
